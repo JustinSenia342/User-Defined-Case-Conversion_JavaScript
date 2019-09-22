796ab311bd26d4b410a52d76424f78231da06f1a
Justin Senia <justinsenia342@gmail.com>
2019-09-22 15:23:44 -0400

    FIX FORMATTING

--- js/ProtoFunctions.js
+++ js/ProtoFunctions.js
@@ -1,15 +1,15 @@
-		function changeCase() {
-		//alert("changecase(): " + document.getElementById("userIn").value);
-		var userString = document.getElementById("userIn").value;
-	
-		var radioIn = document.getElementsByName('changeCase');
-	
-		if (radioIn[0].checked == true)
-			userString = userString.toUpperCase();
-	
-		else if (radioIn[1].checked == true)
-			userString = userString.toLowerCase();
-	
-		document.getElementById("jsOut").value = userString;
-	
-		}
+function changeCase() {
+//alert("changecase(): " + document.getElementById("userIn").value);
+var userString = document.getElementById("userIn").value;
+
+var radioIn = document.getElementsByName('changeCase');
+
+if (radioIn[0].checked == true)
+	userString = userString.toUpperCase();
+
+else if (radioIn[1].checked == true)
+	userString = userString.toLowerCase();
+
+document.getElementById("jsOut").value = userString;
+
+}
